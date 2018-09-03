24e685cbe3904237888e1a4ab7076536fc380370
glennissa <asrbd37@gmail.com>
2018-09-03 14:51:51 +0200

    Calculatrice.ps1

---
+++ Calculatrice.ps1
@@ -0,0 +1,28 @@
+﻿#### script calculatrice #####
+
+Write-Host = "Bienvenue sur la calculatrice Powershell"
+
+while ($true) {
+
+
+
+[int]$number1 = Read-Host "Quel est le premier nombre ?"
+
+[string] $op = Read-Host "quel est l'opérateur"
+
+[int]$number2 = Read-Host "Quel est le second nombre ?"
+
+
+
+
+switch ($op) {
+    "+" { $res = $number1 + $number2 }
+    "-" { $res = $number1 - $number2 }
+    "*" { $res = $number1 * $number2 }
+    "/" { $res = $number1 / $number2 }
+    default { $res = "error"}
+  }
+
+write-host "`nResult >> $res`n"
+}
+
